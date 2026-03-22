1fb68f187b9f59b866571a41193257d39444c07d
shocgu09 <shocguna@gmail.com>
2026-03-23 05:57:26 +0900

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDXJPgdgqDOUnjZam_kOHpaC4peCTJpvWI",
+  authDomain: "dt-club.firebaseapp.com",
+  projectId: "dt-club",
+  storageBucket: "dt-club.firebasestorage.app",
+  messagingSenderId: "1709148758",
+  appId: "1:1709148758:web:acc93145c8aebea4dd09f7"
+};
