1963cd331b5210adf605a17c28d592092f419494
shocgu09 <shocguna@gmail.com>
2026-03-30 16:40:10 +0900

    Revert "어드민: 접속 위치 ip-api.com lang=ko로 한글 표시"
    This reverts commit 6deeb2cffd67ea3e6821c58d382c7505cc0ecbaf.

--- app.js
+++ app.js
@@ -158,10 +158,10 @@ function initAuth() {
         const data = userDoc.data();
         state.currentUserRole = data.role;
         // 마지막 접속 시간 + 위치 갱신
-        fetch('https://ip-api.com/json/?lang=ko&fields=city,regionName')
+        fetch('https://ipapi.co/json/')
           .then(r => r.json())
           .then(d => {
-            const loc = [d.regionName, d.city].filter(Boolean).join(' ') || '';
+            const loc = [d.city, d.region].filter(Boolean).join(', ') || d.country_name || '';
             state.db.collection('users').doc(user.uid).update({ lastSeen: new Date().toISOString(), lastLocation: loc }).catch(() => {});
           })
           .catch(() => {
